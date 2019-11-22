ee5cf2125fc353adc52deb680e7cbc5335a0eadb
joao96 <jvpoletti@gmail.com>
2019-11-22 16:26:53 -0300

    :art: actions initial config

---
+++ bot/actions.py
@@ -0,0 +1,11 @@
+from rasa_sdk import Action
+
+class ActionTest(Action):
+    def name(self):
+        return "action_test"
+
+    def run(self, dispatcher, tracker, domain):
+        try:
+            dispatcher.utter_message("Mensagem enviada por uma custom action.")
+        except ValueError:
+            dispatcher.utter_message(ValueError)
